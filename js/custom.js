1aa34e9841f9f87bc8879afe3fe24baacd480281
Ravindra S. Rathore <rrathore@rightpoint.com>
2020-03-06 23:52:32 +0530

    ds

--- js/custom.js
+++ js/custom.js
@@ -349,6 +349,25 @@ function includeHTML() {
             return;
         }
     }
-}
+    }
+$(document).ready(function () {
+    //  Smoothscroll js
+    //=========================================
+    $("a").on('click', function (event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.attr('target');
+            alert(hash);
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
 })(jQuery);
 
